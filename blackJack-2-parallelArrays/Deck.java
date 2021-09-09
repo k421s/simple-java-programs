ef0580116004280acb0256618967df92debcc7cb
Karlton Suits <karltonsuits@gmail.com>
2021-09-09 12:56:52 -0400

    Add files via upload

---
+++ blackJack-2-parallelArrays/Deck.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class Deck {
+	
+	private int deckSize = 52;
+	
+	
+	private Card[] cardArray = new Card[deckSize];
+	
+	int[] valueArray = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+	private final String[] faceArray = {"2","3","4","5","6","7","8","9","10","Jack", "Queen","King","Ace"};
+	private final String[] suitArray = {"Dimonds", "Hearts","Spaces","Clubs"};
+	
+	//initialize array of objects to whatever
+	//face = 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+	
+	public enum Rank {
+		ACE,
+		TWO,
+		THREE,
+		FOUR,
+		FIVE,
+		SIX,
+		SEVEN,
+		EIGHT,
+		NINE,
+		TEN,
+		JACK,
+		QUEEN,
+		KING;}
+
+	public enum Suit {    
+		CLUBS,
+		SPADES,
+		HEARTS,
+		DIAMONDS;}
+
+
+	public Deck(){
+
+
+
+		int i = 0;
+  for (Suit s : Suit.values()) {
+    for (Rank r : Rank.values()) {
+      cards[i]= new Card(r,s);
+      i++;  // increment i here
+    }
+  }
+}
+		for(int i = 0; i < deckSize; i++){
+			cardArray[i] = new Card(faceArray[i % faceArray.length], suitArray[i % suitArray.length]);
+		}
+		this.shuffle();
+	}
+	
+	//shuffle method
+	public void shuffle(){
+		
+		//random numbers for two values to swap indexes of
+		for(int i = 0; i < 1000; i++){
+			
+			Random rand = new Random();
+			int  a = rand.nextInt(deckSize);
+			int b = rand.nextInt(deckSize);
+			
+			Card temp = new Card(cardArray[a]);
+			cardArray[a] = new Card(cardArray[b]);
+			cardArray[b] = new Card(temp);
+		}
+	}
+	
+	public void sort(){
+		
+	}
+	
+	public String toString(){
+		String deckString = "";
+		for (int i = 0; i < cardArray.length; i++) {
+			deckString = deckString + cardArray[i].toString() + "\n";
+		}
+		
+		return deckString;
+	}	
+	
+	public Card getCard(int index){
+		return cardArray[index];
+	}
+	
+	
+}
